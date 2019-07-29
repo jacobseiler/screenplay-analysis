b474f5e198acd1c283001183d7a420b2c6f213ae
jacobseiler <jseiler@swin.edu.au>
2019-07-30 00:08:19 +1000

    Added classes for lines and scenes.

---
+++ line.py
@@ -0,0 +1,56 @@
+#TODO: Add a ``__repr__`` that is basically "Name: spoken_line."
+
+from character_utils import normalize_name
+
+from textblob import TextBlob
+
+
+class Line(object):
+
+    def __init__(self, character_name, spoken_line):
+
+        self.character_name = character_name
+        self._spoken_line = spoken_line
+
+        sentiment = TextBlob(spoken_line).sentiment
+        self._subjectivity = sentiment.subjectivity
+        self._polarity = sentiment.polarity
+
+    @property
+    def character_name(self):
+        return self._character_name
+
+    @character_name.setter
+    def character_name(self, character_name):
+        # Before we set the name, normalize it. This prevents weird instances of where a
+        # character is called slightly different names across episodes (e.g., 'Jaime
+        # Lannister' versus 'Jaime').
+        character_name = normalize_name(character_name)
+
+        self._character_name = character_name
+
+    @property
+    def spoken_line(self):
+        return self._spoken_line
+
+    @spoken_line.setter
+    def spoken_line(self, spoken_line):
+        self._spoken_line = spoken_line
+
+    @property
+    def subjectivity(self):
+        return self._subjectivity
+
+    @property
+    def polarity(self):
+        return self._polarity
+
+
+    def __repr__(self):
+        """
+        Sets the represenation of a line to simply be the line itself.
+        """
+
+        my_string = f"'{self.spoken_line}'; polarity:{self.polarity}; " \
+                    f"subjectivity:{self.subjectivity}"
+        return my_string
---
+++ scene.py
@@ -0,0 +1,70 @@
+# TODO: Add a __repr__ and __str__ to describe a scene.
+
+class Scene(object):
+
+    def __init__(self, season_num, episode_num):
+
+        self._lines = []
+        self._characters = []
+
+        self._season_num = season_num
+        self._episode_num = episode_num
+
+    @property
+    def lines(self):
+        return self._lines
+
+    @lines.setter
+    def lines(self, lines):
+        self._lines = lines
+
+    @property
+    def characters(self):
+        """
+        list of strings: Name of each character that talks in the scene.
+        """
+        # Go through each line in the scene, and then build a list of all the characters
+        # that spoke.
+        chars_in_scene = []
+
+        for line in self.lines:
+            chars_in_scene.append(line.character_name)
+
+        # Enforce uniqueness.
+        unique_chars = list(set(chars_in_scene))
+
+        return unique_chars
+
+    @characters.setter
+    def characters(self, characters):
+        self._characters = characters
+
+    @property
+    def season_num(self):
+        return self._season_num
+
+    @season_num.setter
+    def season_num(self, season_num):
+        self._season_num = season_num
+
+    @property
+    def episode_num(self):
+        return self._episode_num
+
+    @episode_num.setter
+    def episode_num(self, episode_num):
+        self._episode_num = episode_num
+
+    def __repr__(self):
+
+        my_string = f"s{self.season_num:02d}e{self.episode_num:02} scene"
+
+        return my_string
+
+    def __str__(self):
+
+        my_string = f"Characters in Scene for Season {self.season_num} Episode " \
+                    f"{self.episode_num} is {self.characters}. A total of {len(self.lines)} " \
+                    f"lines were spoken."
+
+        return my_string
